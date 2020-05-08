7c301d2210e4f875588fc280d609c5b8319582c1
Abhay Gupta <gupta94abhay@gmail.com>
2020-05-07 22:52:09 -0700

    adding tf2.1 with cuda10 support

--- tests/test_tf_installation.py
+++ tests/test_tf_installation.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
 
 print ("Tensorflow version is: " + str(tf.__version__))
-sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+print ("Is CUDA Available: ", tf.test.is_gpu_available(cuda_only=True))
+print ("Num of physical devices are: ", len(tf.config.list_physical_devices('GPU')))
